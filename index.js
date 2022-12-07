d279c652daf92ca6ae9fd8de81ae501285ae7da2
EIIinor <ellinor.ljus@utb.ecutbildning.se>
2022-12-07 14:22:10 +0100

    api

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ const cors = require ('cors')
 const bodyParser = require('body-parser')
 const app = express()
 
+
+// middleware
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
+// routes
 const productsController = require('./controllers/productsController')
 app.use('/api/products', productsController)
+app.use('/api/authentucation', require('./controllers/authenticationController'))
 
 
+// initialize
 initMongoDB()
 app.listen(port, () => console.log('WebApi is running on http://localhost:${port}'))
